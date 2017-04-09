b394957eb1b284cf9f211c1cb7334fa8f1f88470
Tristan Lopus <tristan.lopus@gmail.com>
2017-04-08 21:09:27 -0700

    Removed sitc_workforce_creds.php from repo

--- app/appServer/sitc_workforce_creds.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $hostname = 'localhost';
-  $database = 'sitc_workforce';
-  $username = 'root';
-  $password = 'Tko14akM';
-
-  // Stripe API Keys
-  $stripeAPIKey_sk_test = "sk_test_15GXveb8D3IfzP998jR1LmgW"; //SITC secret test API key
-?>
