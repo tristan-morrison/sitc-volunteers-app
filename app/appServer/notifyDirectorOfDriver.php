da5edfa4a6f03a873e865d5ffd8e8440a104f0d5
Developer <tristan.lopus@gmail.com>
2017-04-27 16:53:02 -0700

    first working commit of driver director email notification

--- app/appServer/notifyDirectorOfDriver.php
+++ app/appServer/notifyDirectorOfDriver.php
@@ -1,65 +1,103 @@
 <?php
+/**
+ * This example shows settings to use when sending via Google's Gmail servers.
+ */
 
-  require_once 'sitc_workforce_creds.php';
-  require_once 'PHPMailerAutoload.php';
+//SMTP needs accurate times, and the PHP time zone MUST be set
+//This should be done in your php.ini, but this is how to do it if you don't have access to that
+date_default_timezone_set('Etc/UTC');
 
+require_once __DIR__ . '/../../bower_components/phpmailer/PHPMailerAutoload.php';
 
-  error_log("*** notifyDirectorOfDriver.php IS RUNNING");
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
 
-  $info = (isset($_GET['info'])) ? json_decode($_GET['info'], true) : null;
-  if (!$info) {
-    exit(402); // improper request
-  }
+$info = (isset($_GET['info'])) ? json_decode($_GET['info'], true) : null;
+if (!$info) {
+  exit(402); // improper request
+}
 
-  $name = $info['name'];
-  $email = $info['email'];
-  $phone = $info['phone'];
-  $highSchool = $info['highSchool'];
-  $carpoolSite = $info['carpoolSite'];
+$name = $info['name'];
+$email = $info['email'];
+$phone = $info['phone'];
+$highSchool = $info['highSchool'];
+$carpoolSite = $info['carpoolSite'];
 
-  $mail = new PHPMailer;
-
-  $mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-  $mail->isSMTP();                                      // Set mailer to use SMTP
-  $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-  $mail->SMTPAuth = true;                               // Enable SMTP authentication
-  $mail->Username = 'tristan@summerinthecity.com';                 // SMTP username
-  $mail->Password = 'Tko-14-aaR';                           // SMTP password
-  $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-  $mail->Port = 587;                                    // TCP port to connect to
-
-  $mail->setFrom('tristan@summerinthecity.com', 'Mailer');
-  $mail->addAddress('tech@summerinthecity.com', 'Tech User');     // Add a recipient
-
-  $mail->isHTML(true);                                  // Set email format to HTML
-
-  $mail->Subject = 'Here is the subject';
-
-  $mail->Body = '<html><body>';
-  $mail->Body .= "<h3>ucwords($name) just registered, and they said they can drive</h3>";
-  $mail->Body .= "<h4>Follow up with them to seal the deal!</h4>";
-  $mail->Body .= "<br /><br />Here's their info:<br />";
-  $mail->Body .= "<strong>Email: </strong>$email";
-  $mail->Body .= "<strong>Phone: </strong>$phone";
-  $mail->Body .= "<strong>High School: </strong>$highSchool";
-  $mail->Body .= "<strong>Carpool Site: </strong>$carpoolSite";
-  $mail->Body .= '</html></body>';
+// format data
+ucwords($name);
+$phone = '(' . substr($phone, 0, 3) . ') ' . substr($phone, 3, 3) . '-' . substr($phone, 6);
 
 
-  // $headers = "From: $senderEmail" . "\r\n" . "Reply-To: $senderEmail" . "\r\n";
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
 
-  // Sending email
-  if(!$mail->send()) {
-    error_log("Message failed to send");
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-  } else {
-    echo 'Message has been sent';
-    error_log('Message has been sent');
-  }
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 2;
+
+//Ask for HTML-friendly debug output
+$mail->Debugoutput = 'html';
+
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+// use
+// $mail->Host = gethostbyname('smtp.gmail.com');
+// if your network does not support SMTP over IPv6
+
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "webmaster@summerinthecity.com";
+
+//Password to use for SMTP authentication
+$mail->Password = "sitc+2-0*0/2";
+
+//Set who the message is to be sent from
+$mail->setFrom('tech@summerinthecity.com', 'SITC Tech');
+
+// //Set an alternative reply-to address
+// $mail->addReplyTo('replyto@example.com', 'First Last');
+
+//Set who the message is to be sent to
+$mail->addAddress('tristan@summerinthecity.com', 'Tristan');
+
+//Set the subject line
+$mail->Subject = 'PHPMailer GMail SMTP test';
+
+$message = '<html><body>';
+$message .= "<h3 style='padding-bottom:0px'>$name just registered, and they said they can drive.</h3>";
+$message .= "<h4>Follow up with them to seal the deal!</h4>";
+$message .= "<strong>Here's their info:</strong><br />";
+$message .= "<strong>Email: </strong>$email<br />";
+$message .= "<strong>Phone: </strong>$phone<br />";
+$message .= "<strong>High School: </strong>$highSchool<br />";
+$message .= "<strong>Carpool Site: </strong>$carpoolSite<br /><br />";
+$message .= "<h4>Have a wonderful day.</h4><br />";
+$message .= "<span>All our love,</span><br />";
+$message .= "<span>Tristan & Tristn</span>";
+$message .= '</html></body>';
+
+$mail->msgHTML($message);
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+}
 
 ?>
+
+
 
  <?php
    function sanitize($var) {
