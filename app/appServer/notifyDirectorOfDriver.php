be2b14f8153dc2de8309ee9850d9cb21f75a2148
Tristan Lopus <tristan.lopus@gmail.com>
2017-04-28 08:52:09 -0400

    Added first time volunteer question; added hsGradYear to email

--- app/appServer/notifyDirectorOfDriver.php
+++ app/appServer/notifyDirectorOfDriver.php
@@ -17,14 +17,18 @@ if (!$info) {
   exit(402); // improper request
 }
 
-$name = $info['name'];
+$firstName = $info['firstName'];
+$lastName = $info['lastName'];
 $email = $info['email'];
 $phone = $info['phone'];
 $highSchool = $info['highSchool'];
+$hsGradYear = $info['hsGradYear'];
 $carpoolSite = $info['carpoolSite'];
+$firstTimeTeer = $info['firstTimeTeer'];
 
 // format data
-ucwords($name);
+ucwords($firstName);
+ucwords($lastName);
 $phone = '(' . substr($phone, 0, 3) . ') ' . substr($phone, 3, 3) . '-' . substr($phone, 6);
 
 
@@ -69,19 +73,25 @@ $mail->setFrom('tech@summerinthecity.com', 'SITC Registration');
 
 //Set who the message is to be sent to
 $mail->addAddress('tristan@summerinthecity.com', 'Tristan');
-$mail->addAddress('ben@summerinthecity.com', 'Ben');
+// $mail->addAddress('ben@summerinthecity.com', 'Ben');
 
 //Set the subject line
 $mail->Subject = 'A car-endowed volunteer just registered.';
 
 $message = '<html><body>';
-$message .= "<h3 style='padding-bottom:0px'>$name just registered, and they said they can drive.</h3>";
+$message .= "<h3 style='padding-bottom:0px'>$firstName $lastName just registered, and they said they can drive.</h3>";
 $message .= "<h4>Follow up with them to seal the deal!</h4>";
 $message .= "<strong>Here's their info:</strong><br />";
 $message .= "<strong>Email: </strong>$email<br />";
 $message .= "<strong>Phone: </strong>$phone<br />";
-$message .= "<strong>High School: </strong>$highSchool<br />";
-$message .= "<strong>Carpool Site: </strong>$carpoolSite<br /><br />";
+$message .= "<strong>High School: </strong>$highSchool, class of $hsGradYear<br />";
+$message .= "<strong>Carpool Site: </strong>$carpoolSite<br />";
+if ($firstTimeTeer == 1 || $firstTimeTeer == '1') {
+  $message .= "<strong>Rookie: </strong>This is $firstName's <strong>first time</strong> volunteering, so be sure to give an extra warm and explanatory welcome.<br />";
+}
+else {
+  $message .= "<strong>Veteran: </strong>$firstName <strong>has</strong> volunteered with us before, so don't worry about re-hashing the basics with them<br />";
+}
 $message .= "<h4>Have a wonderful day.</h4><br />";
 $message .= "<span>All our love,</span><br />";
 $message .= "<span>Tristan & Tristn</span>";
