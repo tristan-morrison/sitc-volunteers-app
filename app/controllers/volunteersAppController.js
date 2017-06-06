b592cf3805420f025abac4f3cf9b08e97b05a5dd
Tristan Lopus <tristan.lopus@gmail.com>
2017-06-05 21:55:44 -0600

    Add check for IE and tells user to try different browser, if detected. (Temporary fix until app can be made compatible with IE)

--- app/controllers/volunteersAppController.js
+++ app/controllers/volunteersAppController.js
@@ -1,6 +1,14 @@
 var app = angular.module('volunteersApp')
 
-app.controller('VolunteersAppController', ['$scope', '$log', function($scope, $log) {
+app.controller('VolunteersAppController', ['$scope', '$log', '$mdDialog', function($scope, $log, $mdDialog) {
   $log.log('Hello, world! VolunteersAppController is running!')
 
+  if (navigator.userAgent.indexOf('Trident') > -1) {
+    $mdDialog.show(
+      $mdDialog.alert()
+        .title('Uh, oh: Internet Explorer')
+        .textContent("It looks like you are using Internet Explorer. We're sorry, but our registration form will not work properly on Internet Explorer. Please try a different web browser. (If you don't have another browser on your computer, try a smartphone.)")
+        .ok('Will do!')
+    )
+  }
 }])
